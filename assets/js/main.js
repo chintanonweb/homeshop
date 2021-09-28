6495cb0535256fa9a798a22f4b924929b757cf5d
Chintan Dhokai <chintandhokai97@gmail.com>
2021-09-28 10:23:03 +0530

    data updated

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+(function ($) {
+    "use strict";
+    
+    // Initiate the wowjs
+    $( document ).ready(function() {
+        new WOW().init()
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "73px");
+        } else {
+            $('.nav-bar').removeClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "0");
+        }
+    });
+    
+})(jQuery);
